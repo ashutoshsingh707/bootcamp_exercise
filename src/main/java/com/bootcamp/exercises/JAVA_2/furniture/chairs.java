d6671445a1c0aa54c7e4c894a696faac66f29601
ashutosh <ashutosh.singh@tothenew.com>
2020-02-20 14:48:58 +0530

    Chairs file

---
+++ src/main/java/com/bootcamp/exercises/JAVA_2/furniture/chairs.java
@@ -0,0 +1,44 @@
+package furniture;
+
+import com.bootcamp.exercises.JAVA_2.furniture.chair;
+
+class MetalChairs extends chair
+{
+    {
+        Type = "metal chair";
+        StressProof_= "true";
+        FireProof = "true";
+    }
+    void getDetails()
+    {
+        System.out.println(Type);
+        System.out.println("Stress Proof: "+StressProof_);
+        System.out.println("Fire Proof: "+FireProof);
+    }
+}
+class WoodenChairs extends chair
+{
+    {
+        Type = "Wooden chair";
+        StressProof_= "true";
+        FireProof = "Noooo";
+    }
+    void getDetails()
+    {
+        System.out.println(Type);
+        System.out.println("Stress Proof: "+StressProof_);
+        System.out.println("Fire Proof: "+FireProof);
+    }
+}
+class chairs
+{
+    public static void main(String args[])
+    {
+        System.out.println("****METAL CHAIR DETAIL*********");
+        MetalChairs m=new MetalChairs();
+        m.getDetails();
+        System.out.println("****WOODEN CHAIR DETAIL*********");
+        WoodenChairs w=new WoodenChairs();
+        w.getDetails();
+    }
+}
